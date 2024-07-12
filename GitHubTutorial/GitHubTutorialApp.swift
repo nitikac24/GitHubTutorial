5b901f1f2b16c2596a58fac209f975b53d6051be
Nitika Chakkirala <nitika.chakkirala@gmail.com>
2024-07-12 11:56:20 -0700

    Initial Commit

---
+++ GitHubTutorial/GitHubTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTutorialApp.swift
+//  GitHubTutorial
+//
+//  Created by Scholar on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
